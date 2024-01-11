79e7bba9bea358568e89b5347d269628dc1f9cc8
Adam Monsen <haircut@gmail.com>
2024-01-11 10:53:19 -0800

    apply AGPL to build code

--- book/.internal-build.sh
+++ book/.internal-build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Invoke rake to build _Steadfast Self-Hosting_
+# Copyright (C) 2024  Adam Monsen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##############################################################
+# Build the book, generating various typeset output formats. #
+##############################################################
+
 set -o errexit
 set -o nounset
 set -o pipefail
