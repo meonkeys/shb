30683334e7569be62af97d7736d4c067a05ce6ee
Adam Monsen <haircut@gmail.com>
2024-01-24 19:14:07 -0800

    upgrade asciidoctor-pdf to 2.3.11
    Removes the need for ink_running_content override. Yay!
    
    See: https://github.com/asciidoctor/asciidoctor-pdf/issues/2477

--- book/extensions.rb
+++ book/extensions.rb
@@ -8,25 +8,6 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
     convert_image image_block, relative_to_imagesdir: true, pinned: true
     super
   end
-
-  # Add one invisible character to pages only containing running content.
-  # This allows using `media: prepress` with some printers that don't like these sorta empty pages (e.g. Lulu).
-  #
-  # per Dan Allen:
-  # > The likely culprit is the running content on otherwise empty pages.
-  # > It's possible their analyzer thinks that is invalid for some reason (if it only sees a PDF stamp, which is what the running content is).
-  #
-  # See also:
-  # * discussion https://asciidoctor.zulipchat.com/#narrow/stream/288690-users.2Fasciidoctor-pdf/topic/syntax.20error.20with.20prepress.20PDF.20on.20Lulu
-  # * https://github.com/asciidoctor/asciidoctor-pdf/issues/2476
-  # * https://github.com/asciidoctor/asciidoctor-pdf/issues/2477
-  def ink_running_content(*)
-    (1..page_count).each do |pgnum|
-      go_to_page pgnum
-      update_colors if graphic_state.color_space.empty?
-    end
-    super
-  end
 end
 
 # Allow long words to wrap at slashes.
