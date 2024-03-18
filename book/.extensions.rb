ac9f68be46ad4936da2e559573e597331dcb6c35
Adam Monsen <haircut@gmail.com>
2024-03-18 16:06:35 -0700

    sharpen and darken chapter break image
    Much better!
    
    This is part of my response to a fantastic design review by Bob Nystrom.
    Thank you @munificent !

--- book/.extensions.rb
+++ book/.extensions.rb
@@ -4,7 +4,7 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
   # Add an image at the start of each chapter.
   # see https://docs.asciidoctor.org/pdf-converter/latest/extend/use-cases/#chapter-image
   def ink_chapter_title sect, title, opts
-    image_attrs = { 'target' => 'chapter-break.png', 'pdfwidth' => '100%' }
+    image_attrs = { 'target' => 'chapter-break.svg', 'pdfwidth' => '100%' }
     image_block = ::Asciidoctor::Block.new sect.document, :image, content_model: :empty, attributes: image_attrs
     convert_image image_block, relative_to_imagesdir: true, pinned: true
     super
