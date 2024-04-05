60684d2c8a70a4f840b2a8eaa7d3b5fb02f65fe8
Adam Monsen <haircut@gmail.com>
2024-04-05 16:15:57 -0700

    update Scratch and fix Maintenance notes

--- mario/ansible/roles/services/templates/ops/scratch/custom/webpack.config.js
+++ mario/ansible/roles/services/templates/ops/scratch/custom/webpack.config.js
@@ -1,16 +1,16 @@
 const defaultsDeep = require('lodash.defaultsdeep');
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 // Plugins
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 // PostCss
-var autoprefixer = require('autoprefixer');
-var postcssVars = require('postcss-simple-vars');
-var postcssImport = require('postcss-import');
+const autoprefixer = require('autoprefixer');
+const postcssVars = require('postcss-simple-vars');
+const postcssImport = require('postcss-import');
 
 const STATIC_PATH = process.env.STATIC_PATH || '/static';
 
@@ -62,10 +62,11 @@ const base = {
             }, {
                 loader: 'css-loader',
                 options: {
-                    modules: true,
+                    modules: {
+                        localIdentName: '[name]_[local]_[hash:base64:5]'
+                    },
                     importLoaders: 1,
-                    localIdentName: '[name]_[local]_[hash:base64:5]',
-                    camelCase: true
+                    localsConvention: 'camelCase'
                 }
             }, {
                 loader: 'postcss-loader',
@@ -80,16 +81,43 @@ const base = {
                     }
                 }
             }]
+        },
+        {
+            test: /\.hex$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 16 * 1024
+                }
+            }]
         }]
     },
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
+            new TerserPlugin({
                 include: /\.min\.js$/
             })
         ]
     },
-    plugins: []
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'node_modules/scratch-blocks/media',
+                    to: 'static/blocks-media/default'
+                },
+                {
+                    from: 'node_modules/scratch-blocks/media',
+                    to: 'static/blocks-media/high-contrast'
+                },
+                {
+                    from: 'src/lib/themes/high-contrast/blocks-media',
+                    to: 'static/blocks-media/high-contrast',
+                    force: true
+                }
+            ]
+        })
+    ]
 };
 
 if (!process.env.CI) {
@@ -113,9 +141,10 @@ module.exports = [
         module: {
             rules: base.module.rules.concat([
                 {
-                    test: /\.(svg|png|wav|gif|jpg)$/,
-                    loader: 'file-loader',
+                    test: /\.(svg|png|wav|mp3|gif|jpg)$/,
+                    loader: 'url-loader',
                     options: {
+                        limit: 2048,
                         outputPath: 'static/assets/'
                     }
                 }
@@ -132,15 +161,14 @@ module.exports = [
         },
         plugins: base.plugins.concat([
             new webpack.DefinePlugin({
-                'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"',
+                'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
                 'process.env.DEBUG': Boolean(process.env.DEBUG),
-                'process.env.GA_ID': '"' + (process.env.GA_ID || 'UA-000000-01') + '"'
+                'process.env.GA_ID': `"${process.env.GA_ID || 'UA-000000-01'}"`
             }),
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
-                sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
+                title: 'Scratch 3.0 GUI'
             }),
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'blocksonly'],
@@ -171,14 +199,6 @@ module.exports = [
             new CopyWebpackPlugin({
                 patterns: [
                     {
-                        from: 'node_modules/scratch-blocks/media',
-                        to: 'static/blocks-media'
-                    }
-                ]
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
                         from: 'extensions/**',
                         to: 'static',
                         context: 'src/examples'
@@ -189,7 +209,8 @@ module.exports = [
                 patterns: [
                     {
                         from: 'extension-worker.{js,js.map}',
-                        context: 'node_modules/scratch-vm/dist/web'
+                        context: 'node_modules/scratch-vm/dist/web',
+                        noErrorOnMissing: true
                     }
                 ]
             })
@@ -215,9 +236,10 @@ module.exports = [
             module: {
                 rules: base.module.rules.concat([
                     {
-                        test: /\.(svg|png|wav|gif|jpg)$/,
-                        loader: 'file-loader',
+                        test: /\.(svg|png|wav|mp3|gif|jpg)$/,
+                        loader: 'url-loader',
                         options: {
+                            limit: 2048,
                             outputPath: 'static/assets/',
                             publicPath: `${STATIC_PATH}/assets/`
                         }
@@ -228,16 +250,9 @@ module.exports = [
                 new CopyWebpackPlugin({
                     patterns: [
                         {
-                            from: 'node_modules/scratch-blocks/media',
-                            to: 'static/blocks-media'
-                        }
-                    ]
-                }),
-                new CopyWebpackPlugin({
-                    patterns: [
-                        {
                             from: 'extension-worker.{js,js.map}',
-                            context: 'node_modules/scratch-vm/dist/web'
+                            context: 'node_modules/scratch-vm/dist/web',
+                            noErrorOnMissing: true
                         }
                     ]
                 }),
