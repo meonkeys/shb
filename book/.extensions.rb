bef6e3f7e42c46f77cd26deaf505f08787fba1cb
Adam Monsen <haircut@gmail.com>
2024-04-18 14:39:54 -0700

    use less bare links, more URL macro
    I prefer how these render in all formats, and it makes plain where I am linking excessively in the print version.

--- book/.extensions.rb
+++ book/.extensions.rb
@@ -10,20 +10,3 @@ class ExtendedPDFConverter < (Asciidoctor::Converter.for 'pdf')
     super
   end
 end
-
-# Allow long words to wrap at slashes.
-#
-# See: https://asciidoctor.zulipchat.com/#narrow/stream/288690-users.2Fasciidoctor-pdf/topic/.E2.9C.94.20wrapping.20long.20words.3A.20break.20at.20slash
-Prawn::Text::Formatted::LineWrap.prepend (Module.new do
-  def break_chars(*)
-    '/' + super
-  end
-
-  def word_division_scan_pattern(*)
-    Regexp.union super, /\//
-  end
-
-  def scan_pattern(*)
-    Regexp.union super, /\//
-  end
-end)
